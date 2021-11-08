53882e4ec74e3036739fc82b8f79e787bd789126
Nicolas Rivet <85310076+Nicolas-Rivet@users.noreply.github.com>
2021-11-08 19:03:56 +0100

    Corrigé

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -18,7 +18,7 @@ exports.getOneThing = (req, res, next) => {
       .catch(error => res.status(404).json({ error }));
 };
 
-exports.getAllThing = (req, res, next) => {
+exports.getAllStuff = (req, res, next) => {
     Thing.find()
       .then(things => res.status(200).json(things))
       .catch(error => res.status(400).json({ error }));
